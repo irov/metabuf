875c3e345456041e4004f4a9da47ff813afd54f5
yuriy.levchenko <irov13@mail.ru>
2018-11-10 19:07:19 +0200

    fix config

--- include/config/Metaconfig.hpp
+++ include/config/Metaconfig.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 #ifndef METABUF_MALLOC
-#	include "../../stdex/include/stdex/allocator.h"
-#	define METABUF_MALLOC(S) stdex_malloc(S, "metabuf")
+#include <stdlib.h>
+#define METABUF_MALLOC(S) malloc(S)
 #endif
 
 #ifndef METABUF_FREE
-#	include "../../stdex/include/stdex/allocator.h"
-#	define METABUF_FREE(B, S) stdex_free(B, "metabuf")
-#endif 
+#include <stdlib.h>
+#define METABUF_FREE(B, S) free(B)
+#endif
 
 #ifndef METABUF_MEMCPY
-#	include <algorithm>
-#	define METABUF_MEMCPY(Dest, Src, Size) std::copy((const uint8_t *)Src, (const uint8_t *)Src + Size, (uint8_t *)Dest)
+#include <algorithm>
+#define METABUF_MEMCPY(Dest, Src, Size) std::copy((const uint8_t *)Src, (const uint8_t *)Src + Size, (uint8_t *)Dest)
 #endif
 
 #include <vector>
@@ -20,7 +20,7 @@
 
 namespace Metabuf
 {
-    template<class Type, class Allocator = std::allocator<Type>>
+    template<class Type, class Allocator = std::allocator<Type> >
     using Vector = std::vector<Type, Allocator>;
 
     using Exception = std::exception;
